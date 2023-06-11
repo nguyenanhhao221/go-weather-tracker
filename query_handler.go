2f1b6b751e0c13188fb2eac89910ee42ba5dcae6
Hao Nguyen <hao@haonguyen.tech>
2023-06-12 01:18:40 +0700

    feat(api): add endpoint to get London weather

--- query_handler.go
+++ query_handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"net/url"
@@ -11,18 +12,61 @@ func queryCityHandler(w http.ResponseWriter, r *http.Request) {
 	apiKey := apiCfg.getApiKey()
 
 	city := "London"
+	log.Printf("Getting weather data for %v", city)
 	response, err := getCityWeather(apiKey, city)
 	if err != nil {
-		responseWithJSON(w, response.StatusCode, err)
+		log.Println("Error when getting api city", err)
 	}
-	defer response.Body.Close()
-	responseWithJSON(w, response.StatusCode, response.Body)
+	responseWithJSON(w, 200, response)
 }
 
-func getCityWeather(apiKey string, city string) (*http.Response, error) {
+type CityWeatherRes struct {
+	Coord struct {
+		Lon float64 `json:"lon"`
+		Lat float64 `json:"lat"`
+	} `json:"coord"`
+	Weather []struct {
+		ID          int    `json:"id"`
+		Main        string `json:"main"`
+		Description string `json:"description"`
+		Icon        string `json:"icon"`
+	} `json:"weather"`
+	Base string `json:"base"`
+	Main struct {
+		Temp      float64 `json:"temp"`
+		FeelsLike float64 `json:"feels_like"`
+		TempMin   float64 `json:"temp_min"`
+		TempMax   float64 `json:"temp_max"`
+		Pressure  int     `json:"pressure"`
+		Humidity  int     `json:"humidity"`
+	} `json:"main"`
+	Visibility int `json:"visibility"`
+	Wind       struct {
+		Speed float64 `json:"speed"`
+		Deg   int     `json:"deg"`
+	} `json:"wind"`
+	Clouds struct {
+		All int `json:"all"`
+	} `json:"clouds"`
+	Dt  int `json:"dt"`
+	Sys struct {
+		Type    int    `json:"type"`
+		ID      int    `json:"id"`
+		Country string `json:"country"`
+		Sunrise int    `json:"sunrise"`
+		Sunset  int    `json:"sunset"`
+	} `json:"sys"`
+	Timezone int    `json:"timezone"`
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Cod      int    `json:"cod"`
+}
+
+func getCityWeather(apiKey string, city string) (*CityWeatherRes, error) {
 	parsedUrl, err := url.Parse(baseUrl)
 	if err != nil {
 		log.Printf("Failed to parse baseUrl: %v", err)
+		return nil, err
 	}
 
 	params := url.Values{}
@@ -30,6 +74,21 @@ func getCityWeather(apiKey string, city string) (*http.Response, error) {
 	params.Add("q", city)
 
 	parsedUrl.RawQuery = params.Encode()
-	log.Println(parsedUrl.String())
-	return http.Get(parsedUrl.String())
+
+	weatherResp, err := http.Get(parsedUrl.String())
+	if err != nil {
+		return nil, err
+	}
+	defer weatherResp.Body.Close()
+
+	decoder := json.NewDecoder(weatherResp.Body)
+	cityWeatherData := CityWeatherRes{}
+	decodeErr := decoder.Decode(&cityWeatherData)
+
+	if decodeErr != nil {
+		log.Println("Error: Failed to decode json from OpenWeatherAPI", decodeErr)
+		return nil, decodeErr
+	}
+
+	return &cityWeatherData, nil
 }
